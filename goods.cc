7746c2bbfddbf3e5efc381e47812989752266caf
myblueton <myblueton@gmail.com>
2019-06-12 14:10:29 +0900

    Update goods.cc
    class 내에서 initialize ....   Public { void SetGoodInfo(int index, int price) { goods_index_=index;....}

--- goods.cc
+++ goods.cc
@@ -1,10 +1,10 @@
 #include "./goods.h"
 
 // Set member variable of goods with given index and price values
-void Goods::SetGoodsInfo(int index, int price) {
-  goods_index_ = index;
-  goods_price_ = price;
-}
+//void Goods::SetGoodsInfo(int index, int price) {  // class 내에서 initialize ....   Public { void SetGoodInfo(int index, int price) { goods_index_=index;....}
+//  goods_index_ = index;
+//  goods_price_ = price;
+//}
 
 // Return index information
 int Goods::GetIndex() {
